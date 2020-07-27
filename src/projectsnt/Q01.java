48ab32888755d6356fc4f337ce1e9ac24f965381
apple <apple@apples-imac-6.attlocal.net>
2020-07-27 18:53:17 -0400

    project update

--- src/projectsnt/Q01.java
+++ src/projectsnt/Q01.java
@@ -4,15 +4,15 @@ public class Q01 {
 	
 	/*
 		                              Warm Up - 15 Minutes
-		    
-		1) Create a package, name it as "finalprojects"
-		2) Create a package, name it as "questions"
-		3) Create a class, name it as Q01
-		4) Get personal names from user
-		5) Create 'unique' id numbers for every personnel
-		6) If some personnel has completely same names put "." at the end of the second name
-		7) When user enters 'q' it will end the program 
-		7) Print the personnel names and unique ids like => {John Walker=202035, Mark Twain=202036, John Walker.=202037} 
+
+		1) Create a package, name it as "projectsnt"
+		2) Create a class, name it as Q01
+		3) Get personal names from user
+		4) Create 'unique' id numbers for every personnel
+		5) If some personnel has completely same names, ask personnel to select a nick name 
+		   and use nickname at the end of the name.
+		6) When user enters 'q' it will end the program 
+		7) Print the personnel names and unique ids like => {John Walker=202035, Mark Twain=202036, John Walker Nick=202037} 
     */
 
 	public static void main(String[] args) {
